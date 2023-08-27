4620f76358e909d2b782ddf056dde62c40f4c9b3
sandile05 <106535890+sandile05@users.noreply.github.com>
2023-08-27 19:58:17 +0200

    category carousel slider

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+document.addEventListener("DOMContentLoaded", function () {
+    let swiper = new Swiper(".mySwiper", {
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+});
