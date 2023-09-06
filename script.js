d6246e9eefd981d43b1dec80cf59c98d02ed11aa
sandile05 <106535890+sandile05@users.noreply.github.com>
2023-09-06 17:46:16 +0200

    home section with bg image

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let swiper = new Swiper(".mySwiper", {
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
-    });
-});
+
