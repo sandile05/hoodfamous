7c800455704f8ec80c93a76ee53e4ec03cd7c63e
sandile05 <106535890+sandile05@users.noreply.github.com>
2023-09-07 19:38:05 +0200

    eplore talents carousel

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+document.querySelectorAll('.navbar-nav a.nav-link').forEach(function(navLink) {
+    navLink.addEventListener('click', function() {
+      // Close the navbar by toggling the collapse class
+      const navbar = document.querySelector('.navbar-collapse');
+      if (navbar.classList.contains('show')) {
+        navbar.classList.remove('show');
+      }
+    });
+  });
+
 
