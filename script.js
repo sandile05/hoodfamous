d9fadb44b8e7cf41f3cc2580c7975da2b02e96ce
sandile05 <106535890+sandile05@users.noreply.github.com>
2023-09-11 16:02:51 +0200

    back to top button styling

--- script.js
+++ script.js
@@ -12,3 +12,21 @@ const tooltips = document.querySelectorAll(".tt");
 tooltips.forEach((t) => {
   new bootstrap.Tooltip(t);
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const backToTopButton = document.getElementById("back-to-top");
+
+  // Add a click event listener to scroll back to the "home" section
+  backToTopButton.addEventListener("click", function () {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+  });
+
+  // Show or hide the button based on the scroll position
+  window.addEventListener("scroll", function () {
+      if (window.scrollY > 300) {
+          backToTopButton.classList.add("visible");
+      } else {
+          backToTopButton.classList.remove("visible");
+      }
+  });
+});
