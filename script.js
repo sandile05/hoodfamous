3d6f425e2860826dcadcf713b7b843b376a128bf
sandile05 <106535890+sandile05@users.noreply.github.com>
2023-09-11 15:07:30 +0200

    contact us section

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-document.querySelectorAll('.navbar-nav a.nav-link').forEach(function(navLink) {
-    navLink.addEventListener('click', function() {
-      // Close the navbar by toggling the collapse class
-      const navbar = document.querySelector('.navbar-collapse');
-      if (navbar.classList.contains('show')) {
-        navbar.classList.remove('show');
-      }
-    });
+document.querySelectorAll(".navbar-nav a.nav-link").forEach(function (navLink) {
+  navLink.addEventListener("click", function () {
+    // Close the navbar by toggling the collapse class
+    const navbar = document.querySelector(".navbar-collapse");
+    if (navbar.classList.contains("show")) {
+      navbar.classList.remove("show");
+    }
   });
+});
+
+const tooltips = document.querySelectorAll(".tt");
+tooltips.forEach((t) => {
+  new bootstrap.Tooltip(t);
+});
